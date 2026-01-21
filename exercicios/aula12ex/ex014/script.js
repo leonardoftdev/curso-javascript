dced0eff7c2b456da5cf126e5f1e4294feb87833
Leonardo Silva <leonardoftdev@gmail.com>
2026-01-21 15:05:21 -0300

    update: concluido exercicio de JavaScript

---
+++ exercicios/aula12ex/ex014/script.js
@@ -0,0 +1,16 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.jpg'
+        document.body.style.background = '#bdd187'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.jpg'
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#013042'
+    }
+}
